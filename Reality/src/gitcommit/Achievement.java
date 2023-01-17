c6e1649a923785104196d2176031729d3c57c305
sryadav1619 <yadavsantosh1619@gmail.com>
2023-01-17 11:25:55 -0800

    Commit1
    Always makes good friends

---
+++ Reality/src/gitcommit/Achievement.java
@@ -0,0 +1,10 @@
+package gitcommit;
+
+public class Achievement {
+
+	public static void main(String[] args) {
+        
+		System.out.println("Always remember that we have to take decision quickly");
+	}
+
+}
